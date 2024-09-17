7c3f0c8fd65bb538ee32238046d2de19f5389101
Alwin6 <93875388+Alwin6@users.noreply.github.com>
2024-09-17 12:52:29 +0200

    commented out error
    commented out error message which always triggers

--- src/main/java/com/alba/tracer/Scene.java
+++ src/main/java/com/alba/tracer/Scene.java
@@ -82,7 +82,7 @@ public class Scene extends JPanel implements KeyListener {
             renderer = new Render(hdrLoader);
 
         } catch (IOException e) {
-            System.out.println("HDR file not found or failed to load, using default background.");
+            // System.out.println("HDR file not found or failed to load, using default background.");
             // Initialize without HDRLoader
             useHDR = false;
             renderer = new Render(null);
